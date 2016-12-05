1737fd26e19c6acbf6f8386344ef3a8d0f0fbf61
Nick Brooks <nbrooks@thirtyseven.com.au>
2016-12-05 23:52:31 +1030

    Video #8

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,6 @@
+function comments(state = [], action) {
+    console.log("comments");
+    return state;
+}
+
+export default comments;
---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,6 @@
+function posts(state = [], action) {
+    console.log("piddly posts");
+    return state;
+}
+
+export default posts;
